502f10bbe87d5106d343914be32a81b57a8d8d62
Diego Alzate <diego.alzate@kuepa.com>
2021-01-21 18:44:22 -0500

    adding fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 interface IApp {}
 
 const App: React.FC<IApp> = () => {
-  return <div>Diego Fernando Alzate</div>
+  return (
+    <div>
+      <div>By Diego</div>
+      Your <FontAwesomeIcon icon={faCoffee} /> is hot and ready!
+    </div>
+  )
 }
 
 export default App
